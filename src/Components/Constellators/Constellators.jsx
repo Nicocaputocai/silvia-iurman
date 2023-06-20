fb45aedb99242bb6f89bee2bb7bc62ea85235436
adriangonzalez-dev <adriangonzalez.dev90@gmail.com>
2023-06-20 16:55:33 -0300

    constellator finished

--- src/Components/Constellators/Constellators.jsx
+++ src/Components/Constellators/Constellators.jsx
@@ -3,8 +3,56 @@ import { Container, Row } from 'react-bootstrap'
 import { Silvia, SilviaCuadrada } from '../../assets/images'
 import { SimpleCard } from './SimpleCard'
 import { HelmetPage } from '../components'
+import AOS from 'aos'
+import 'aos/dist/aos.css';
+import { useEffect } from 'react'
+import UserDataServices from '../../Services/UserServices'
+import { useState } from 'react'
+import { PageLoader } from '../components/PageLoader'
+
+const constellation_ = {
+  firstName: 'Silvia',
+  lastName: 'Iurman',
+  avatar: SilviaCuadrada
+}
 
 export const Constellators = () => {
+  const [constellators, setConstellators] = useState({
+    data: [],
+    isLoading: true
+  })
+
+  const getConstellators = async () => {
+    try {
+      const { data } = await UserDataServices.getConstellators()
+      setConstellators({
+        ...constellators,
+        data: data.constellators,
+      })
+    } catch (error) {
+      console.log(error)
+    } finally {
+      setConstellators({
+        ...constellators,
+        isLoading: false
+      })
+    }
+  }
+
+  UserDataServices
+
+  useEffect(()=>{
+    AOS.init({
+      duration: 1000
+    })
+    getConstellators()
+  },[])
+  console.log(constellators)
+
+  if(constellators.isLoading) return (
+    <PageLoader/>
+  )
+
   return (
     <>
     <HelmetPage section='Consteladores' content='Consteladores - Nuevas Constelaciones Familiares Argentina'/>
@@ -12,36 +60,23 @@ export const Constellators = () => {
     <Container className='text-center'>
       <h1 className='mt-3 mb-3 textColor'>Consteladores acreditados</h1>
 
-    <Row xs={2} md={4} className="g-4 mb-3">
+    {/* Esto solo es a modo de prueba! */}
+    <Row xs={1} sm={2} md={4} className="g-4 mb-3">
       {Array.from({ length: 4 }).map((_, idx) => (
-        <SimpleCard image={Silvia} key={idx}/>
+        <SimpleCard constellation={constellation_} key={idx}/>
       ))}
     </Row>
 
-    <Row xs={2} md={4} className="g-4 mb-3">
-      {Array.from({ length: 4 }).map((_, idx) => (
-        <SimpleCard image={SilviaCuadrada} key={idx}/>
-      ))}
-    </Row>
-
-     <Row xs={2} md={5} className="g-4 mb-3">
-      {Array.from({ length: 5 }).map((_, idx) => (
-        <SimpleCard 
-          image={Silvia} 
-          className='rounded-circle w-100 h-auto'
-          key={idx}
-        />
-      ))}
-    </Row>
-
-    <Row xs={2} md={5} className="g-4 mb-3">
-      {Array.from({ length: 5 }).map((_, idx) => (
-        <SimpleCard 
-          image={SilviaCuadrada}
-          key={idx}
-        />
-      ))}
-    </Row>
+    {/* al subir se deben descomentar estas lineas */}
+    {/* 
+        <Row xs={1} sm={2} md={4} className="g-4 mb-3">
+          {
+          constellators.data.map((constellation, idx) => (
+          <SimpleCard constellation={constellation} key={idx}/>
+          }
+        </Row>
+    
+     */}
    
     </Container>
     </>
--- src/Components/Constellators/SimpleCard.jsx
+++ src/Components/Constellators/SimpleCard.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import { Card, Col } from 'react-bootstrap'
 
-export const SimpleCard = ({image, className=''}) => {
+export const SimpleCard = ({constellation}) => {
   return (
-    <Col>
-          <Card>
-            <Card.Img className={`img-fluid ${className}`} src={image}/>
-            <Card.Body className='text-center'>
-              <Card.Title>Nombre y Apellido</Card.Title>
+    <Col className='mb-4'>
+          <Card data-aos="fade-up" className='shadow-sm rounded-2 position-relative'>
+            <Card.Img className='img-fluid object-fit-cover' src={constellation.avatar}/>
+            <Card.Body className='text-center position-absolute bottom-0 bg-white opacity-75 w-100'>
+              <Card.Title
+              className='fw-bold fs-5'
+              >
+              {`${constellation.firstName} ${constellation.lastName}`}
+              </Card.Title>
 
             </Card.Body>
           </Card>
