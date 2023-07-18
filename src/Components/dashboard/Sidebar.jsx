492ca01b1954733e19b42fa463d5c16991b147e6
adriangonzalez-dev <adriangonzalez.dev90@gmail.com>
2023-07-18 13:23:45 -0300

    preview content

--- src/Components/dashboard/Sidebar.jsx
+++ src/Components/dashboard/Sidebar.jsx
@@ -25,6 +25,29 @@ export const Sidebar = ({setContent}) => {
   return (
     <div className={`me-auto ${styles.sidebar_size}`}>
     <Accordion defaultActiveKey="0">
+    <Accordion.Item eventKey="1">
+        <Accordion.Header>Modulos En Directo</Accordion.Header>
+        <Accordion.Body>
+          <ListGroup defaultActiveKey="#link1">
+              
+              {
+                modules.data.map(module => (
+                module.typeModule === 'sincronico' &&
+                <ListGroup.Item key={module._id}>
+                    <Card>
+                        <Card.Header>{module.title}</Card.Header>
+                        <Card.Body className='d-flex justify-content-between'>
+                        <Button variant="warning" disabled={habilited.includes(module._id)} onClick={() => addToCheckout(module, TYPE_PURCHASE.MODULE)}>Comprar</Button>
+                        <Button variant="primary" disabled={!habilited.includes(module._id)} onClick={() => {setContent({module, link:true})}}>Continuar</Button>
+                        </Card.Body>
+                    </Card>
+                </ListGroup.Item>
+                ))
+              }
+          
+        </ListGroup>
+        </Accordion.Body>
+      </Accordion.Item>
       <Accordion.Item eventKey="0">
         <Accordion.Header>Modulos Grabados</Accordion.Header>
         <Accordion.Body>
@@ -68,29 +91,7 @@ export const Sidebar = ({setContent}) => {
         </ListGroup>
         </Accordion.Body>
       </Accordion.Item>
-      <Accordion.Item eventKey="1">
-        <Accordion.Header>Modulos En Directo</Accordion.Header>
-        <Accordion.Body>
-          <ListGroup defaultActiveKey="#link1">
-              
-              {
-                modules.data.map(module => (
-                module.typeModule === 'sincronico' &&
-                <ListGroup.Item key={module._id}>
-                    <Card>
-                        <Card.Header>{module.title}</Card.Header>
-                        <Card.Body className='d-flex justify-content-between'>
-                        <Button variant="warning" disabled={habilited.includes(module._id)} onClick={() => addToCheckout(module, TYPE_PURCHASE.MODULE)}>Comprar</Button>
-                        <Button variant="primary" disabled={!habilited.includes(module._id)}>Continuar</Button>
-                        </Card.Body>
-                    </Card>
-                </ListGroup.Item>
-                ))
-              }
-          
-        </ListGroup>
-        </Accordion.Body>
-      </Accordion.Item>
+      
     </Accordion>
           
       </div>
