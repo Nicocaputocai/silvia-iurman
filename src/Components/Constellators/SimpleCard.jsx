272a9c9499ee2c3f1f9908529c2a7397cdd67ab4
adriangonzalez-dev <adriangonzalez.dev90@gmail.com>
2023-07-17 21:15:30 -0300

    rounded none

--- src/Components/Constellators/SimpleCard.jsx
+++ src/Components/Constellators/SimpleCard.jsx
@@ -5,7 +5,7 @@ export const SimpleCard = ({constellation}) => {
   return (
     <Col className='mb-4' >
           <Card data-aos="fade-up" className='shadow-sm rounded-2 position-relative'>
-            <Card.Img className='w-100 h-100 object-fit-cover img-fluid rounded-circle' src={`https://api-silvia.divisioncode.net.ar/img/${constellation.avatar}`}/>
+            <Card.Img className='w-100 h-100 object-fit-cover img-fluid' src={`https://api-silvia.divisioncode.net.ar/img/${constellation.avatar}`}/>
             <Card.Body className='text-center position-absolute bottom-0 bg-white opacity-75 w-100'>
               <Card.Title
               className='fw-bold fs-5'
